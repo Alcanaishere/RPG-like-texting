aa0be9503c64ebe95f808afdc187ddaa3ceeadb3
Alcanaishere <a1canaph@gmail.com>
2026-03-20 19:08:56 +0900

    Add files via upload
    click to next message

--- test_script.js
+++ test_script.js
@@ -1,18 +1,51 @@
-const message = "Hello! World";
+const message_list = [
+  "Hello! World",
+  "This is a test of texting system like RPG",
+  "I hope this works successfully :D",
+  "There's nothing after this message",
+  "I said nothing huh",
+  "nothing fr",
+  "nothing",
+  "nothing beats",
+  "nothing beats a",
+  "nothing beats a jet-2 holiday✈",
+];
+
+let typing = false;
+let messageIndex = 0;
+let charIndex = 0;
+
 const element = document.getElementById("text");
-const num = 1;
 
-let index = 0;
+function type() {
+  const message = message_list[messageIndex] ?? "";
 
+  typing = true;
 
-
-
-function type(i) {
-  if (index < message.length) {
-    element.textContent += message[index];
-    index++;
-    setTimeout(type, 100); 
+  if (charIndex < message.length) {
+    element.textContent += message[charIndex];
+    charIndex++;
+    setTimeout(type, 50);
+  } else {
+    typing = false;
   }
 }
 
-type()
+document.addEventListener("click", () => {
+  if (typing) {
+    element.textContent = message_list[messageIndex];
+    charIndex = message_list[messageIndex].length; 
+    typing = false;
+    return;
+  }
+
+  messageIndex++;
+  if (messageIndex >= message_list.length) messageIndex = 0;
+
+  element.textContent = "";
+  charIndex = 0;
+
+  type();
+});
+
+type();
